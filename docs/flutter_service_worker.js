7e6c6a3002ab1937884d8703aba4d07aaf1cc5c4
brunohsmartins <bruno.bhsm@hotmail.com>
2025-07-07 16:38:21 -0300

    Deploy do build web para GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "859ca80f634a776b96ff5d1eba76ddee
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ff44610f52de7d2ba4e2e45c8cb05bcf",
+"flutter_bootstrap.js": "d84ad6055dda36d96892006168a73f50",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
